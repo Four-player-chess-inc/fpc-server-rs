7eca088100cb1c5802dd75d74dcb82b3afa82207
h04x <liquid.doctrine@gmail.com>
2021-02-16 02:31:37 +0300

    init

---
+++ src/proto.rs
@@ -0,0 +1,84 @@
+use serde::{Deserialize, Serialize};
+
+// Handshake //////////////////////////////////
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Protocol {
+    SupportedVersion(Vec<String>),
+    Version(String)
+}
+
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum GetInfoError {
+    UnspecifiedError { description: String }
+}
+
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum GetInfo {
+    Request {},
+    Ok { protocol: Protocol },
+    Error(GetInfoError)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Server {
+    name: String,
+    version: String
+}
+
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ConnectError {
+    UnsupportedProtocolVersion { description: String },
+    UnspecifiedError { description: String }
+}
+
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Connect {
+    Client { name: String, version: String, protocol: Protocol },
+    Ok { server: Server },
+    Error(ConnectError)
+}
+
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Handshake {
+    GetInfo(GetInfo),
+    Connect(Connect)
+}
+
+// MatchmakingQueue ///////////////////////////
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PlayerRegisterError {
+    BadName { description: String },
+    AlreadyRegistered { description: String },
+    QueueIsFull { description: String },
+    UnspecifiedError { description: String },
+}
+
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PlayerRegister {
+    Name(String),
+    Ok { id: u8 },
+    Error(PlayerRegisterError),
+}
+
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum MatchmakingQueue {
+    PlayerRegister(PlayerRegister),
+    PlayerLeave {},
+}
+
+#[serde(rename_all = "snake_case")]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Pdu {
+    Handshake(Handshake),
+    MatchmakingQueue(MatchmakingQueue),
+
+}
